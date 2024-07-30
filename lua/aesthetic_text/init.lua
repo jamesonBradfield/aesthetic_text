50318de547a7fbabd5db2d93d19b620d181dce6d
JamesonBradfield <jamesonbradfield@gmail.com>
2024-07-30 11:40:08 -0600

    semi working prototype (vertical slice)

--- lua/aesthetic_text/init.lua
+++ lua/aesthetic_text/init.lua
@@ -1,13 +1,12 @@
 local M = {}
 local current_icons = {}
-local keystrokes = {}
-local is_aesthetic_insert
-M.start_aesthetic_insert = function(icon_name)
-	is_aesthetic_insert = true
-	current_icons = require("aesthetic_text." .. icon_name)
-	for key, value in pairs(current_icons) do
-		print(key, value)
-	end
+M.aesthetic_menu = function()
+	-- TODO: we need to get a table of all lua modules in this folder that aren't init.lua
+
+	current_icons = require("aesthetic_text.icons")
+	-- for key, value in pairs(current_icons) do
+	-- 	print(key, value)
+	-- end
 	-- we need to map this out, I have done this in telekasten note
 	-- vim.on_key(function(key, typed)
 	-- 	if vim.api.nvim_get_mode()["mode"] == "i" then
@@ -24,17 +23,96 @@ M.start_aesthetic_insert = function(icon_name)
 	-- 		vim.api.nvim_buf_set_lines(0, current_line_number - 1, current_line_number, true, { new_line })
 	-- 	end
 	-- end)
+	--
+
+	local Popup = require("nui.popup")
+	local Layout = require("nui.layout")
+	local Input = require("nui.input")
+	local preview = Popup({
+		border = "double",
+	})
+	local output = ""
+
+	local input = Input({
+		position = "50%",
+		size = {
+			width = 20,
+		},
+		border = {
+			style = "single",
+			text = {
+				top = "[Howdy?]",
+				top_align = "center",
+			},
+		},
+		win_options = {
+			winhighlight = "Normal:Normal,FloatBorder:Normal",
+		},
+	}, {
+		prompt = "> ",
+		default_value = "Hello",
+		on_close = function()
+			print("Input Closed!")
+		end,
+		on_submit = function(value)
+			print("Input Submitted: " .. value)
+			local cursor_pos = vim.api.nvim_win_get_cursor(0)
+			local row, col = cursor_pos[1], cursor_pos[2]
+			vim.api.nvim_buf_set_text(0, row - 1, col, row - 1, col, { output })
+		end,
+		on_change = function(value)
+			output = ""
+			for file_key, file_value in current_icons do
+				for c in value:gmatch(".") do
+					local icon
+					if c ~= " " then
+						c = string.lower(c)
+						icon = current_icons[file_key][c] -- look up the icon for each character
+					else
+						icon = c
+					end
+					-- process the icon as needed
+					output = output .. icon -- concatenate to the new string
+				end
+			end
+		end,
+	})
+
+	local layout = Layout(
+		{
+			position = "50%",
+			size = {
+				width = 80,
+				height = "30%",
+			},
+		},
+		Layout.Box({
+			Layout.Box(input, { size = "10%" }),
+			Layout.Box(preview, { size = "90%" }),
+		}, { dir = "col" })
+	)
+
+	local current_dir = "row"
+
+	preview:map("n", "r", function()
+		if current_dir == "col" then
+			layout:update(Layout.Box({
+				Layout.Box(preview, { size = "40%" }),
+				Layout.Box(input, { size = "60%" }),
+			}, { dir = "row" }))
+
+			current_dir = "row"
+		else
+			layout:update(Layout.Box({
+				Layout.Box(input, { size = "60%" }),
+				Layout.Box(preview, { size = "40%" }),
+			}, { dir = "col" }))
+
+			current_dir = "col"
+		end
+	end, {})
+
+	layout:mount()
 end
-M.stop_aesthetic_insert = function()
-	is_aesthetic_insert = false
-	vim.on_key(nil)
-end
--- toggle doesn't work yet
-M.toggle_aesthetic_insert = function(icon_name)
-	if is_aesthetic_insert then
-		M.stop_aesthetic_insert()
-	else
-		M.start_aesthetic_insert(icon_name)
-	end
-end
+
 return M
