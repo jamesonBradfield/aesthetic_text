91fe373c9d88dd323a29f1530139dc1122ae4ded
JamesonBradfield <jamesonbradfield@gmail.com>
2024-08-07 20:51:08 -0600

    edited readme.md

--- lua/aesthetic_text/init.lua
+++ lua/aesthetic_text/init.lua
@@ -2,29 +2,7 @@ local M = {}
 local current_icons = {}
 M.aesthetic_menu = function()
 	-- TODO: we need to get a table of all lua modules in this folder that aren't init.lua
-
-	current_icons = require("aesthetic_text.icons")
-	-- for key, value in pairs(current_icons) do
-	-- 	print(key, value)
-	-- end
-	-- we need to map this out, I have done this in telekasten note
-	-- vim.on_key(function(key, typed)
-	-- 	if vim.api.nvim_get_mode()["mode"] == "i" then
-	-- 		local current_line_number, cursor_position = unpack(vim.api.nvim_win_get_cursor(0))
-	-- 		local line_till_cursor = vim.api.nvim_buf_get_text(
-	-- 			0,
-	-- 			current_line_number - 1,
-	-- 			0,
-	-- 			current_line_number - 1,
-	-- 			cursor_position,
-	-- 			{}
-	-- 		)[1]
-	-- 		local current_line = vim.api.nvim_buf_get_lines(0, current_line_number - 1, current_line_number, true)[1]
-	-- 		vim.api.nvim_buf_set_lines(0, current_line_number - 1, current_line_number, true, { new_line })
-	-- 	end
-	-- end)
-	--
-
+	current_icons = require("aesthetic_text.icons.nf-md-alpha__box")
 	local Popup = require("nui.popup")
 	local Layout = require("nui.layout")
 	local Input = require("nui.input")
@@ -41,7 +19,7 @@ M.aesthetic_menu = function()
 		border = {
 			style = "single",
 			text = {
-				top = "[Howdy?]",
+				top = "[Input Text]",
 				top_align = "center",
 			},
 		},
@@ -62,18 +40,18 @@ M.aesthetic_menu = function()
 		end,
 		on_change = function(value)
 			output = ""
-			for file_key, file_value in current_icons do
-				for c in value:gmatch(".") do
-					local icon
-					if c ~= " " then
-						c = string.lower(c)
-						icon = current_icons[file_key][c] -- look up the icon for each character
-					else
-						icon = c
-					end
-					-- process the icon as needed
-					output = output .. icon -- concatenate to the new string
+			-- TODO: add capital letter keys to nf-md-alpha__box
+			-- so Barbar will produce 󰬉󰬈󰬙󰬉󰬈󰬙 with all but the B nf-md-alpha__box_outline
+			for c in value:gmatch(".") do
+				local icon
+				if c ~= " " then
+					c = string.lower(c)
+					icon = current_icons[c] -- look up the icon for each character
+				else
+					icon = c
 				end
+				-- process the icon as needed
+				output = output .. icon -- concatenate to the new string
 			end
 		end,
 	})
@@ -82,7 +60,7 @@ M.aesthetic_menu = function()
 		{
 			position = "50%",
 			size = {
-				width = 80,
+				width = 20,
 				height = "30%",
 			},
 		},
